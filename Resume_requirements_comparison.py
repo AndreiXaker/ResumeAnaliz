dca8cc150e42e9c071254a7e1b7a6ba66fe65a37
Dima343316 <72628630+Dima343316@users.noreply.github.com>
2024-02-28 17:36:26 +0300

    Update Resume_requirements_comparison.py

--- Resume_requirements_comparison.py
+++ Resume_requirements_comparison.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from AnalyzatorResumeModel import LLMRequest_GetResult_base
 from openai import OpenAI
 from api_seatable import SeatableSettings
 import httpx
-from keys import openai_key, seatable_api_prompts, proxy_url
+from keys import openai_key, seatable_api_prompts, proxyurl
 import PyPDF2
+from test import LLMRequest_GetResult_base
 
 proxies = {
-    "http://": proxy_url,
-    "https://": proxy_url,
+    "http://": proxyurl,
+    "https://": proxyurl,
 }
 
 seatable_settings = SeatableSettings("https://cloud.seatable.io", seatable_api_prompts)
@@ -18,11 +18,10 @@ openai_client = OpenAI(http_client=httpx.Client(proxies=proxies), api_key=(opena
 
 
 # Функция для создания объекта класса LLMRequest_GetResult_base
-def create_llm_request(candidate_resume, job_summary):
-    # Здесь следует использовать ваши параметры и загруженные файлы для создания объекта
+def create_llm_request(candidate_resume):
     openaiclient = openai_client
     seasettings = seatable_settings
-    return LLMRequest_GetResult_base(openaiclient, seasettings, candidate_resume, job_summary)
+    return LLMRequest_GetResult_base(openaiclient, seasettings, candidate_resume)
 
 
 
@@ -33,13 +32,7 @@ st.set_page_config(
 )
 
 
-def convert_pdf_to_text(file_pdf):
-    pdf_reader = PyPDF2.PdfFileReader(file_pdf)
-    text = ""
-    for page_num in range(pdf_reader.numPages):
-        page = pdf_reader.getPage(page_num)
-        text += page.extractText()
-    return text
+
 
 st.markdown(
     """
@@ -51,19 +44,19 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-st.markdown('**Выберите файл для загрузки требований к вакансии :**')
+st.markdown('**Выберите файл для загрузки требований к вакансии  и кандидатов :**')
 
-file_resume_candidate = st.file_uploader("Загрузить файл. Принимаемый формат - .JSON", type=['json'])
-st.markdown("**Выберите файл для загрузки резюме кандидата  :**")
-file_resume_job_summary = st.file_uploader("Загрузить файл. Принимаемый формат - .PDF", type=[ 'pdf'])
+file_resume_candidate_and_vacansy = st.file_uploader("Загрузить файл. Принимаемый формат - .JSON", type=['json'])
+
 
 
 
 if st.button('Process Files'):
-    if file_resume_job_summary and file_resume_candidate:
+    if file_resume_candidate_and_vacansy:
             with st.spinner():
-                lml_request = create_llm_request(file_resume_candidate, file_resume_job_summary)
+                lml_request = create_llm_request(file_resume_candidate_and_vacansy)
                 result = lml_request.query()
                 st.write(result)
+                print('Обработка данных завершена')
                 # result_number = find_elements_in_square_brackets(result)
                 # st.title(f' Вывод оценки сходства кандидата с требованиями вакансии {result_number}')
